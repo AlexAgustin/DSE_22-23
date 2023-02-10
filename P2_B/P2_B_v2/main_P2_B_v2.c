47d9aa6dff0dc9791dfa31dc0993c225d2b96729
Amanda Sin <81495532+Andyuks@users.noreply.github.com>
2023-02-10 22:56:46 +0100

    Update main_P2_B_v2.c

--- P2_B/P2_B_v2/main_P2_B_v2.c
+++ P2_B/P2_B_v2/main_P2_B_v2.c
@@ -1,17 +1,7 @@
 /*
-Cronometro mediante leds.
-Control de temporizador por interrupcion.
-Se utilizan los siguientes leds:
- * LED3 para decimas de segundo
- * LED5 para segundos
- * LED7 para decenas de segundos
- * LED9 para minutos
-Control del inicio del temporizador mediante encuesta del pulsador RD13 (S4).
-Control del temporizador mediante pulsadores por interrupcion.
-Se usan los siguientes botones:
- * RA7 (S5) para parar o continuar con el cronometro
- * RD7 (S6) para reiniciar el cronometro
-
+Esperar una cantidad determinada de milisegundos (ms) y microsegundos (us)
+ * LED3 para indicar cantidad de ms a esperar superior a lo contemplado
+ * LED5 para indicar cantidad de us a esperar superior a lo contemplado
 Autores: Alex Agustin y Amanda Sin
 Fecha: 09/02
 */
@@ -33,18 +23,20 @@ int main()
 	inic_leds();	// Inicializacion leds: sentido y valor inicial.
     
 	Nop();
-    Nop();
-    delay_ms(5);
-    delay_ms(25);
-    delay_ms(150);
-    delay_ms(425); // se sale del rango maximo
+	Nop();
+	// Espera de milisegundos 
+	delay_ms(5);
+	delay_ms(25);
+	delay_ms(150);
+	delay_ms(425); // se sale del rango maximo
     
-    Nop();
-    Nop();
-    delay_us(400);
-    delay_us(1000);
-    delay_us(20000);
-    delay_us(420000); //se sale del rango maximo
+	Nop();
+	Nop();
+	// Espera de microsegundos 
+	delay_us(400);
+	delay_us(1000);
+	delay_us(20000);
+	delay_us(420000); //se sale del rango maximo
 
 	return (0);
 }
