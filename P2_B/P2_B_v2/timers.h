f5f594f8a6926290001564e91ac9bb95b0b95c9a
Amanda Sin <81495532+Andyuks@users.noreply.github.com>
2023-02-10 23:01:53 +0100

    Update timers.h

--- P2_B/P2_B_v2/timers.h
+++ P2_B/P2_B_v2/timers.h
@@ -1,9 +1,6 @@
-// Exporta funciones invocadas en otroos modulos
+// Exporta funciones invocadas en otros modulos
 
 // funciones
-void inic_Timer7 ();
-void cronometro();
-void inic_crono();
 void inic_Timer9(unsigned long ciclos);
 void delay_ms(unsigned int ms);
 void delay_us(unsigned int us);
