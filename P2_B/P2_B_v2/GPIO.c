c79a3c5be88524ae0ab83a089eef28f5e13defb5
Amanda Sin <81495532+Andyuks@users.noreply.github.com>
2023-02-10 23:06:40 +0100

    Update GPIO.c

--- P2_B/P2_B_v2/GPIO.c
+++ P2_B/P2_B_v2/GPIO.c
@@ -1,31 +1,13 @@
 /*
-Funciones de inicializacion y tratamiento de pulsadores y leds
+Funciones de inicializacion y tratamiento de leds
 
-Egileak: Izaskun
-Data: Febrero 2020
+Autores: Alex y Amanda
+Fecha: febrero 2023
 */
 
 
 #include "p24HJ256GP610A.h"
 #include "commons.h"
-
-void inic_pulsadores ()
-{
-	// Definir pines AN16 - AN31 como pines digitales para evitar problemas
-	AD1PCFGH = 0xffff;
-       /* Bastaria con definir como digital el pin AN23 que es el que corresponde 
-	a RA7 (S5 y D10): AD1PCFGH = 0b 0000 0000 1000 0000 = 0x0080
-	Si se deja como pin analogico da problemas, ya que detecta que
-	el pulsador S5 esta siempre pulsado aunque no sea asi
-	*/
-   	
-	// Definir los pines de los pulsadores S4, S5 y S6 como entrada
-	// S6(RD7), S5 (RA7),  S4(RD13)
-
-	TRISDbits.TRISD7 = 1;
-    TRISAbits.TRISA7 = 1;
-	TRISDbits.TRISD13 = 1;
-}
 
 void inic_leds()
 {
