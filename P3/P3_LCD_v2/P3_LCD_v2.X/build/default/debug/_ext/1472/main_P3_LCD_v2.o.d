aadc9da58812b326aa875586a11c7d0deb8a81df
AlexAgustin <aagustin002@ikasle.ehu.eus>
2023-02-17 15:34:01 +0100

    clase viernes 17/02

---
+++ P3/P3_LCD_v2/P3_LCD_v2.X/build/default/debug/_ext/1472/main_P3_LCD_v2.o.d
@@ -0,0 +1,10 @@
+build/default/debug/_ext/1472/main_P3_LCD_v2.o:  \
+../main_P3_LCD_v2.c  \
+ ../oscilator.h  \
+../GPIO.h  \
+../timers.h  \
+../commons.h  \
+../LCD.h  \
+../memoria.h  \
+ ../CN.h  \
+../utilidades.h 
---
+++ P3/P3_LCD_v2/timers.c
@@ -0,0 +1,230 @@
+/* 
+Por un lado, contiene las funciones asociadas al modulo T9 (inicializacion) y 
+a las esperas de un determinado numero de milisegundos y microsegundos.
+
+Ademas, contiene las funciones asociadas al modulo T7 (inicializacion e interrupcion) 
+y a su correspondiente cronometro (inicializacion y gestion del paso del tiempo).
+
+Autores: Alex Agustin y Amanda Sin
+Fecha: Febrero 2023
+
+*/
+#include "p24HJ256GP610A.h"
+#include "commons.h"
+#include "memoria.h"
+#include "utilidades.h"
+#include "LCD.h"
+
+#define LCD_LINE1 0
+#define LCD_DATA1 1
+#define LCD_LINE2 2
+#define LCD_DATA2 3
+
+void cronometro();
+
+//Variables globales
+int inicializar_crono = 0;
+unsigned int mili,deci,seg,min;
+
+
+// inicializacion del timer 9
+void inic_Timer9(unsigned long ciclos){
+    TMR9 = 0 ; 	// Inicializar el registro de cuenta
+    if (ciclos < 65535) { //65535 es el maximo numero de ciclos que entra en un tamaño de 2^16 (tamaño de PR) 
+         T9CONbits.TCKPS = 0;	// escala del prescaler 00
+         PR9 =  ciclos-1 ;	// Periodo del timer con prescaler 00
+    }else{
+        if ((ciclos/8) < 65535){
+            T9CONbits.TCKPS = 1;	// escala del prescaler 01
+            PR9 =  (ciclos/8)-1 ;	// Periodo del timer con prescaler 01
+        }else{
+            if ((ciclos/64) < 65535){
+                T9CONbits.TCKPS = 2;	// escala del prescaler 10
+                PR9 =  (ciclos/64)-1 ;	// Periodo del timer con prescaler 10
+            }else{
+                if ((ciclos/256) < 65535) {
+                    T9CONbits.TCKPS = 3;	// escala del prescaler 11
+                    PR9 =  (ciclos/256)-1 ;	// Periodo del timer con prescaler 11
+                }
+            }
+        }
+    }
+    T9CONbits.TCS = 0;	// reloj interno
+    T9CONbits.TGATE = 0;	// Deshabilitar el modo Gate
+    IFS3bits.T9IF = 0;      // Puesta a 0 del flag IF del temporizador 9
+    T9CONbits.TON = 1;	// puesta en marcha del timer
+}
+
+//Espera un determinado numero de milisegundos
+void Delay_ms(unsigned int ms){
+    unsigned long ciclos = (unsigned long)ms * (unsigned long)40000; //el numero de ciclos equivale a ms * 40000
+    
+    if(ciclos < 16776960){// 65535 * 256. Valor maximo que puede aceptar el temporizador con el mayor prescaler
+        inic_Timer9(ciclos);// inicializa el T9
+        
+        while(!IFS3bits.T9IF); // espera a que el temporizador indique que ha finalizado
+        IFS3bits.T9IF = 0; // se marca la interrupcion como atendida
+        T9CONbits.TON = 0; // apagar el temporizador
+    }else{ // Valor de tiempo de espera superior a lo contemplado
+        LATAbits.LATA4=!LATAbits.LATA4; // Se conmuta el LED D7 (RA4)
+        while(1); //espera infinita para advertir del error
+    }
+}
+
+//Espera un determinado numero de microsegundos
+void Delay_us(unsigned int us){
+    unsigned long ciclos = (unsigned long)us * (unsigned long)40; //el numero de ciclos equivale a us * 40
+    
+    if (ciclos < 16776960){// 65535 * 256. Valor maximo que puede aceptar el temporizador con el mayor prescaler
+        inic_Timer9(ciclos);// inicializa el T9
+        
+        while(!IFS3bits.T9IF); // espera a que el temporizador indique que ha finalizado
+        IFS3bits.T9IF = 0; // se marca la interrupcion como atendida
+        T9CONbits.TON = 0; // apagar el temporizador
+    }else{ // Valor de tiempo de espera superior a lo contemplado
+        LATAbits.LATA5=!LATAbits.LATA5; // Se conmuta el LED D8 (RA5)
+        while(1); //espera infinita para advertir del error
+    }
+}
+
+void inic_Timer7 ()
+{
+    //Inicializar modulo T7
+    TMR7 = 0 ; 	// Inicializar el registro de cuenta
+    PR7 =  50000-1 ;	// Periodo del timer
+		// Queremos que cuente 10 ms.
+		// Fosc= 80 MHz (vease Inic_oscilator()) de modo que
+		// Fcy = 40 MHz (cada instruccion dos ciclos de reloj)
+		// Por tanto, Tcy= 25 ns para ejecutar una instruccion
+		// Para contar 10 ms se necesitan 400.000 ciclos.
+    T7CONbits.TCKPS = 1;	// escala del prescaler 01
+    T7CONbits.TCS = 0;	// reloj interno
+    T7CONbits.TGATE = 0;	// Deshabilitar el modo Gate
+    
+    IEC3bits.T7IE = 1;      // habilitacion de la interrupcion general de T7
+    IFS3bits.T7IF = 0;      // Puesta a 0 del flag IF del temporizador 7
+    
+    T7CONbits.TON = 1;	// encender el timer
+}	
+
+
+
+void _ISR_NO_PSV _T7Interrupt()
+{
+    //Han pasado 10 milesimas de segundo
+    mili +=10; //se suman 10 milesimas de segundo
+    IFS3bits.T7IF = 0;      // Puesta a 0 del flag IF del temporizador 7
+}
+
+
+void inic_crono()	
+// Inicializacion de las variables del cronometro: 
+// milesimas de segundo (mili), decimas de segundo (deci), segundos (seg), minutos (min)
+{
+	mili=0;
+    deci=0;
+    seg=0;
+    min=0;
+}
+
+
+void cronometro()	
+// control del tiempo: actualiza cada 10 ms
+// inicializar cronometro: si el flag inicializar_crono esta activado, inicializa el cronometro
+{
+    if(inicializar_crono)
+    {
+        //el flag inicializar_crono esta activado
+        //Puesta a 0 de las variables del cronometro
+        inic_crono(); //inicializa el cronometro
+
+        //Puesta a 0 de lo mostrado en la pantalla
+        copiar_FLASH_RAM((unsigned char*) Mens_LCD_6, 1); //Copiar a memoria RAM la segunda linea (puesta a 0)
+        line_2(); //nos posicionamos en la segunda linea
+        puts_lcd((unsigned char*) Ventana_LCD[1], 16); // Llevar al LCD la segunda linea, desde RAM (puesta a 0)
+
+        inicializar_crono = 0; //puesta a 0 del flag inicializar_crono
+    }
+
+    // actualiza las variables del cronometro y modifica el texto en pantalla y los leds segun corresponda
+    if (mili>=100){ //cada 100 milesimas de segundo
+        deci+=1; //se suma una decima
+        mili-=100; //reset milesimas
+        LATAbits.LATA0=!LATAbits.LATA0; //conmutar LED D3
+
+        if (deci>=10){ //cada 10 decimas de segundo
+            seg+=1; //se suma 1 segundo
+            deci-=10; //reset decimas
+            LATAbits.LATA2=!LATAbits.LATA2; //conmuntar LED D5
+            
+            if (seg>=60){ //cada vez que pasen 60 segundos
+                min+=1; //se suma 1 minuto
+                seg-=60; //reset segundos
+                LATAbits.LATA6=!LATAbits.LATA6; //conmuntar LED D9
+                conversion_tiempo(&Ventana_LCD[1][posmin],min); //asignacion del valor de los minutos en la posicion correspondiente de la linea a mostrar en pantalla  
+            }
+            conversion_tiempo(&Ventana_LCD[1][posseg],seg); //asignacion del valor de los segundos en la posicion correspondiente de la linea a mostrar en pantalla
+        }
+        conversion_tiempo(&Ventana_LCD[1][posds], deci*10); //asignacion del valor de las decimas de segundo en la posicion correspondiente de la linea a mostrar en pantalla
+        Ventana_LCD[1][poscs]=' '; // Poner un 0 en la posicion asociada a las centesimas de segundo
+
+        //line_2(); //nos posicionamos en la segunda linea
+        //puts_lcd((unsigned char*) Ventana_LCD[1], 16); // Llevar al LCD la segunda linea
+    }
+}
+
+void inic_Timer5 ()
+{
+    //Inicializar modulo T5
+    TMR5 = 0 ; 	// Inicializar el registro de cuenta
+    PR5 =  12499 ;	// Periodo del timer ((2.5*40000)/8)-1
+		// Queremos que cuente 2.5 ms.
+		// Fosc= 80 MHz (vease Inic_oscilator()) de modo que
+		// Fcy = 40 MHz (cada instruccion dos ciclos de reloj)
+		// Por tanto, Tcy= 25 ns para ejecutar una instruccion
+		// Para contar 2.5 ms se necesitan 100.000 ciclos.
+    T5CONbits.TCKPS = 1;	// escala del prescaler 01
+    T5CONbits.TCS = 0;	// reloj interno
+    T5CONbits.TGATE = 0;	// Deshabilitar el modo Gate
+    
+    IEC1bits.T5IE = 1;      // habilitacion de la interrupcion general de T5
+    IFS1bits.T5IF = 0;      // Puesta a 0 del flag IF del temporizador 5
+    
+    T5CONbits.TON = 1;	// encender el timer
+}	
+
+void _ISR_NO_PSV _T5Interrupt()
+{
+    static unsigned int estado_LCD = 0;
+    static int i = 0;
+    switch (estado_LCD){
+        case LCD_LINE1: //Posicionamiento en la primera linea
+            lcd_cmd(0x80);  	// Set DDRAM address (@0)
+            estado_LCD = LCD_DATA1; // Cambio de estado, suguiente: envio de datos de la primera linea
+            break;
+        case LCD_DATA1: // Envio de los datos de la primera linea
+            lcd_data(Ventana_LCD[0][i]); //Primero se manda uno de los datos de ventana
+            if (i<15){
+                i++; // Si no es la ultima iteracion, se incrementa la posicion
+            }else if (i == 15) {
+                i=0; //Si es la ultima iteracion, se vuelve a la posicion 0 y...
+                estado_LCD = LCD_LINE2; // y cambiamos de estado, siguiente: posicionamiento en la linea 2
+            }
+            break;
+        case LCD_LINE2: //Posicionamiento en la segunda linea
+            lcd_cmd(0xC0);  	// Set DDRAM address (@40)
+            estado_LCD = LCD_DATA2; // Cambio de estado, suguiente: envio de datos de la segunda linea
+            break;
+        case LCD_DATA2: // Envio de los datos de la segunda linea
+            lcd_data(Ventana_LCD[1][i]);
+            if (i<15){
+                i++; // Si no es la ultima iteracion, se incrementa la posicion
+            }else if (i == 15) {
+                i=0; //Si es la ultima iteracion, se vuelve a la posicion 0 y...
+                estado_LCD = LCD_LINE1; // y cambiamos de estado, siguiente: posicionamiento en la linea 1
+            }
+            break;
+    }
+    
+    IFS1bits.T5IF = 0;      // Puesta a 0 del flag IF del temporizador 5
+}
