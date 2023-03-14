da8d37ab73abf76b8170ed1345576b657b6c8cba
AlexAgustin <aagustin002@ikasle.ehu.eus>
2023-03-14 17:44:37 +0100

    CAD_v1 funciona

--- P5/P5_CAD_v1/utilidades.c
+++ P5/P5_CAD_v1/utilidades.c
@@ -40,38 +40,23 @@ void conversion_tiempo (unsigned char * dir, unsigned int val)
 // En dir estara el caracter de mas peso y en dir+3 el de menos peso
 void conversion_ADC (unsigned char * dir, unsigned int val)
 {
-    unsigned char dig;
+    unsigned int dig;
+    int i,divisor = 1000;
     //int i;
     if (val>1023) {
         while(1);    
     } else { //bulce
-        /*dig=val;
-        for(i=0; i<4; i++){
-            a=dig/pow(10,(3-i));
+        dig=val;
+        for(i=0; i<3; i++){
+            dig=dig/divisor;
             
-        }*/
-        
-        dig=val/1000;
-        dig=tabla_carac[dig];
-        *dir=dig;
-        
-        dir++;
-        
-        dig=val%1000;
-        dig=dig/100;
-        dig=tabla_carac[dig];
-        *dir=dig;
-        
-        dir++;
-        
-        dig=val%100;
-        dig=dig/10;
-        dig=tabla_carac[dig];
-        *dir=dig;
-        
-        dir++;
-        
-        dig=val%10;
+            dig=tabla_carac[dig];
+            *dir=dig;
+            dir++;
+            dig=val%divisor;
+            divisor/=10;
+        }
+        dig=dig/divisor;
         dig=tabla_carac[dig];
         *dir=dig;
     }
